57d5139d09d9abdbd042055d7d3ba4fb3cb9d36d
Wang Tao <thor.wang@sjtu.edu.cn>
2018-05-28 10:20:39 +0800

    modify token filter

--- src/main/java/filter/TokenFilter.java
+++ src/main/java/filter/TokenFilter.java
@@ -56,24 +56,9 @@ public class TokenFilter implements Filter{
 		WebApplicationContext wac = WebApplicationContextUtils.getRequiredWebApplicationContext(fConfig.getServletContext());
 		tokenDao= (TokenDao) wac.getBean("tokenDao");
 		uris=new ArrayList<String>();
-		uris.add("/track/rest/app/user/clientLogin");
-		uris.add("/track/rest/app/user/clientSignup");
-		uris.add("/track/rest/app/feed/feedAround");
-		uris.add("/track/rest/app/feed/getPublicFeedAfterTime");
-		uris.add("/track/rest/app/user/getHomeInfo");
-		uris.add("/track/rest/app/user/getFollowing");
-		uris.add("/track/rest/app/user/getFollower");
-		uris.add("/track/rest/app/user/queryPersonalInfo");
-		uris.add("/track/rest/app/feed/commentList");
-		uris.add("/track/rest/app/user/getPortraitUrl");
-		uris.add("/track/rest/app/user/searchUser");
-		uris.add("/track/rest/app/feed/searchFeed");
-		uris.add("/track/rest/app/feed/getFeedsNotLoggedIn");
-		uris.add("/track/rest/app/feed/getBigPhotoUrls");
-		uris.add("/track/rest/app/user/getBigPortraitUrl");
-		uris.add("/track/rest/app/feed/getPublicFeedBeforeTime");	
-		
-		uris.add("/track/rest/app/user/modifyPersonalInfo");
+		// add a URI into uris to go through filter if it come from app and you need ensure the uri has a token
+		//uris.add("/track/rest/app/user/clientLogin");
+	
 
 	}
 
